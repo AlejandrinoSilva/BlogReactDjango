7f7e3b66c89063a0463ac4e81658a22daf6d5268
programmer <programmer@dhcppc14>
2019-10-18 17:54:28 -0300

    Se va acomodando

---
+++ backend/blogs/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from blogs.models import Blogs
+
+class BlogsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Blogs
+        fields = '__all__'
